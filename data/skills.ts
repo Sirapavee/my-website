b35acb4af86cf0d09cfa7b71547d5dc223b1edea
Sirapavee <sirapavee@gmail.com>
2021-05-30 15:03:58 +0700

    New data directory

---
+++ data/skills.ts
@@ -0,0 +1,41 @@
+const SKILLS: Object[] = [
+
+    {
+        id: 'technical',
+        skills: {
+            'Programming Languages': [
+                'Python', 'JS', 'TS', 
+                'HTML', 'CSS', 'Java', 'C++', 'C#'
+            ],
+            'Industrial Knowledge': [
+                'ML', 'DL', 'IP',
+                'CV', 'Web Dev'
+            ],
+            'Frameworks': [
+                'React', 'Next.js', 'GANs'
+            ],
+            'Tools': [
+                'Figma', 'MS Word', 'MS Powerpoint'
+            ],
+        }
+    },
+    {
+        id: 'language',
+        skills: {
+            'Language': [
+                'Thai', 'English'
+            ]
+        }
+    },
+    {
+        id: 'soft',
+        skills: {
+            'Soft skill': [
+                'Leadership', 'Problem Solving', 'Teamwork', 'Time management',
+                'Critical Thinking', 'Adaptability', 'Creativity'
+            ]
+        }
+    }
+]
+
+export { SKILLS }
